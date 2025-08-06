e85be453d780b81222df4b23c16711f160ae9f20
Clément Omnès <clement.mns.pro@gmail.com>
2025-08-06 12:03:15 +0200

    feat: add GMT component to display current time and date with timezone

---
+++ src/components/layouts/header/header-gmt.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { useLocale } from "next-intl";
+import type { Locale } from "@/types/locales";
+import { fallbackLocale } from "@/types/locales";
+import { useEffect, useState } from "react";
+
+export default function GMT() {
+  const locale = (useLocale() as Locale) || fallbackLocale;
+  const [now, setNow] = useState<Date>(new Date());
+  const timeZone = "Europe/Paris";
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setNow(new Date());
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  const time = now.toLocaleTimeString(locale, {
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    timeZone,
+  });
+  const date = now.toLocaleDateString(locale, {
+    weekday: "short",
+    month: "short",
+    day: "numeric",
+    timeZone,
+  });
+
+  const offset =
+    new Date()
+      .toLocaleTimeString("en-US", {
+        timeZone,
+        timeZoneName: "shortOffset",
+      })
+      .match(/GMT([+-]\d+)/)?.[1] || "0";
+  const displayedTimeZoneName = `GMT${offset}`;
+
+  return (
+    <p className="text-sm font-medium whitespace-nowrap font-mono">{`${displayedTimeZoneName} / ${time} - ${date}`}</p>
+  );
+}
--- src/components/layouts/header/header.tsx
+++ src/components/layouts/header/header.tsx
@@ -1,9 +1,11 @@
 import { SettingsDialog } from "@/components/settings/settings-dialog";
+import GMT from "./header-gmt";
 
 export default function Header() {
   return (
-    <header className="w-full select-none">
-      <div className="container max-w-5xl mx-auto px-4 py-3 flex items-center justify-end">
+    <header className="w-full">
+      <div className="container max-w-5xl mx-auto px-4 py-3 flex items-center justify-between">
+        <GMT />
         <SettingsDialog />
       </div>
     </header>
