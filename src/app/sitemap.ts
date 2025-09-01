712ece03baf46c3beccae7b0fea2ad9249d217a1
Clément Omnès <clement.mns.pro@gmail.com>
2025-09-01 08:50:42 +0200

    refactor: remove default URL entry from sitemap because that's a redirection page

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,12 +5,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
 
   return [
     {
-      url: baseUrl,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 1,
-    },
-    {
       url: `${baseUrl}/en`,
       lastModified: new Date(),
       changeFrequency: "monthly",
